70617bce3f9ce2cc1812d8fb7b07629068c5c4e4
frake23 <dimsacom@gmail.com>
2021-03-06 01:08:42 +0300

    competence section completed

--- components/home_sections/competence_section.js
+++ components/home_sections/competence_section.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import styles from './CompetenceSection.module.scss';
 
 
 export default function CompetenceSection() {
 
+    const [competence, setCompetence] = useState([]);
+    useEffect(() => {
+        fetch('/api/competence')
+            .then(res => res.json())
+            .then(competence => setCompetence(competence));
+    }, [fetch]);
+
     return (
-        <div className="d-flex flex-column py-4 align-items-center bg-gradient">
-            <span className="h2-text text-white">
-                Чем мы занимаемся?
-            </span>
+        <div className="d-flex flex-column py-5 bg-gradient text-center">
+            <div className="container">
+                <span className="h2-text text-white">
+                    Чем мы занимаемся?
+                </span>
+                <div className="d-flex flex-md-row flex-column justify-content-around mt-5">
+                    {
+                        competence.map(item =>
+                            <a className={`d-flex flex-column align-items-center mb-md-0 mb-4 text-decoration-none ${styles.competenceItem}`}>
+                                <img
+                                    src={`/data/competence/${item.iconName}`}
+                                    height={70}
+                                    width={70}
+                                    alt={item.name}
+                                />
+                                <span className="h4-text text-white mt-3">{item.name}</span>
+                            </a>
+                        )
+                    }
+                </div>
+            </div>
+
         </div>
     );
 }
