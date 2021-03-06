096da0252b57f81d4232597b1c5f48507b4a6613
ekinohito <zhenyagur9@gmail.com>
2021-03-06 23:24:17 +0300

    fixed

--- components/poster.js
+++ components/poster.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function Poster({poster, height}) {
     return (
-        <div className="px-2 h-100 bg-light-grey">
+        <div className="p-1 p-md-3 p-xl-4 h-100 bg-light-grey">
             <img
                 src={`/data/posters/${poster.iconName}`}
                 width="auto"
                 height={height}
                 alt={poster.id}
+                style={{borderRadius: '20px'}}
             />
         </div>
 
--- components/slider.js
+++ components/slider.js
@@ -5,7 +5,14 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function Slider({children}) {
 
     return (
-        <Carousel centerMode centerSlidePercentage={40}>
+        <Carousel
+            centerMode
+            centerSlidePercentage={33}
+            showThumbs={false}
+            showStatus={false}
+            showIndicators={false}
+            autoPlay={true}
+            selectedItem={1}>
             {children}
         </Carousel>
     )
