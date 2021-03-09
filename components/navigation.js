d196b7918ca0ce81120147c2ae4a4e3b0a2ed67f
ekinohito <zhenyagur9@gmail.com>
2021-03-10 00:03:29 +0300

    replaced a with next/links

--- components/navigation.js
+++ components/navigation.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Link from "next/link";
 import {useNavigation} from "../context/navigation";
 import styles from "./Navigation.module.scss";
 import {animated, config, useTransition} from "react-spring";
-import useLockBodyScroll from "../hooks/useLockBodyScroll";
 
 const sections = [
-    {name: "Главная", href: "#"},
-    {name: "Афиша", href: "#poster"},
-    {name: "Чем мы занимаемся?", href: "#competence"},
-    {name: "Резиденты", href: "#resident"},
-    {name: "Галерея", href: "#gallery"},
-    {name: "Контакты", href: "#contacts"},
-    {name: "О нас!", href: "/about"}
+    {name: "Главная", href: "/#"},
+    {name: "Афиша", href: "/#poster"},
+    {name: "Чем мы занимаемся?", href: "/#competence"},
+    {name: "Резиденты", href: "/#resident"},
+    {name: "Галерея", href: "/#gallery"},
+    {name: "Контакты", href: "/#contacts"},
+    {name: "О нас!", href: "/about#"}
 ]
 
 
@@ -34,32 +33,35 @@ export default function Navigation() {
     });
     return transition.map(({item, key, props}) =>
         item ?
-                <animated.div key={key} style={props} className={`position-fixed bg-nav-purple h-100 p-4 ${styles.navigation}`}>
-                    <div className="d-flex flex-column">
-                        <div className="align-self-end">
-                            <button className="bg-transparent" onClick={() => setShowNav(false)}>
-                                <img src="/assets/icons/cross.png" alt="cross" width={25} height={25}/>
-                            </button>
-                        </div>
-                        <span className="h3-text text-white">
+            <animated.div key={key} style={props}
+                          className={`position-fixed bg-nav-purple h-100 p-4 ${styles.navigation}`}>
+                <div className="d-flex flex-column">
+                    <div className="align-self-end">
+                        <button className="bg-transparent" onClick={() => setShowNav(false)}>
+                            <img src="/assets/icons/cross.png" alt="cross" width={25} height={25}/>
+                        </button>
+                    </div>
+                    <span className="h3-text text-white">
                             Навигация
                         </span>
-                        <div className="d-flex flex-column mt-2">
-                            {
-                                sections.map((section, index) =>
-                                    <a
-                                        className={`text-decoration-none py-2 px-3 subtitle-text ${styles.navItem}`}
-                                        href={section.href}
-                                        key={index}
-                                        onClick={() => setShowNav(false)}
-                                    >
-                                        {section.name}
-                                    </a>
-                                )
-                            }
-                        </div>
+                    <div className="d-flex flex-column mt-2">
+                        {
+                            sections.map((section, index) =>
+                                        <Link
+                                            href={section.href}
+                                            key={index}
+                                            onClick={() => setShowNav(false)}
+                                        >
+                                            <span
+                                                className={`text-decoration-none px-3 py-2 subtitle-text ${styles.navItem}`}>
+                                                {section.name}
+                                            </span>
+                                    </Link>
+                            )
+                        }
                     </div>
-                </animated.div>
+                </div>
+            </animated.div>
             :
             null
     )
