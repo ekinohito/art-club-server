cd37389fc68bcbfe19a7c7cbd81790b6ae3a87a9
frake23 <dimsacom@gmail.com>
2021-03-10 17:31:28 +0300

    refactoring

---
+++ pages/api/partners.js
@@ -0,0 +1,9 @@
+import competence from "../../data/competence.json";
+
+export default (req, res) => {
+    switch (req.method){
+        case "GET":
+            res.status(200).json(competence);
+            return;
+    }
+}
