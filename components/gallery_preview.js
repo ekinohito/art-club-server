29ef78ea839dbb2a34c202a6365e7b844a1748a9
ekinohito <zhenyagur9@gmail.com>
2021-03-13 00:28:07 +0300

    fixed gallery background

--- components/gallery_preview.js
+++ components/gallery_preview.js
@@ -3,13 +3,13 @@ import styles from './GalleryPreview.module.scss'
 
 export default function GalleryPreview({preview, link, children}) {
     return (
-        <div className="col-4 p-2 pt-4 ">
-            <a href={link} className={styles.recentWork} style={{
-                backgroundImage: `url("/data/gallery/${preview}")`}} target="_blank">
-                <button className={`btn btn-outline-light`}>{children}</button>
-            </a>
+        <div className="col-4 p-0">
+            <div className="m-2 mt-4 overflow-hidden" style={{borderRadius: "20px"}}>
+                <a href={link} className={styles.recentWork} style={{
+                    backgroundImage: `url("/data/gallery/${preview}")`}} target="_blank">
+                    <button className={`btn btn-outline-light`}>{children}</button>
+                </a>
+            </div>
         </div>
-
-
     );
 }
