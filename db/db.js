784a917fe2d7c91a2efd5b9c0f141a155e3e5049
ekinohito <zhenyagur9@gmail.com>
2021-03-18 21:34:34 +0300

    refactored db and init db

--- db/db.js
+++ db/db.js
@@ -5,59 +5,59 @@ const previews = require('./sql/previews')
 const residents = require('./sql/residents');
 const posters = require('./sql/posters');
 const users = require('./sql/users');
+const util = require("util");
+const DEBUG = false;
 
 let db = new sqlite3.Database('./db.db');
+let closure =
+    (method) =>
+        (sql, order) =>
+            (params) => {
+                if (DEBUG) console.log(method, sql, order, params)
+                return util.promisify((...args) =>
+                    db[method](...args))(sql, (order)?order.map(value => params[value]):undefined)
+            }
+
+let run = closure("run")
+let get = closure("get")
+let all = closure("all")
 
 module.exports = {
-    createQuotes: () => new Promise(resolve => db.run(quotes.createQuotesSQL, err => {resolve()})),
-    createPartners: () => new Promise(resolve => db.run(partners.createPartnersSQL, err => {resolve()})),
-    createPreviews: () => new Promise(resolve => db.run(previews.createPreviewsSQL, err => {resolve()})),
-    createResidents: () => new Promise(resolve => db.run(residents.createResidentsSQL, err => {resolve()})),
-    createPosters: () => new Promise(resolve => db.run(posters.createPostersSQL, err => {resolve()})),
-    createUsers: () => new Promise(resolve => db.run(users.createUsersSQL, err => {resolve()})),
+    createQuotes: run(quotes.createQuotesSQL),
+    createPartners: run(partners.createPartnersSQL),
+    createPreviews: run(previews.createPreviewsSQL),
+    createResidents: run(residents.createResidentsSQL),
+    createPosters: run(posters.createPostersSQL),
+    createUsers: run(users.createUsersSQL),
 
 
-    deleteQuotes: () => db.run(quotes.deleteQuotesSQL),
-    deleteOneQuotes: (quote) => db.run(quotes.deleteOnePartnersSQL, [quote.id]),
-    selectQuotes: () => new Promise((resolve) =>
-        db.all(quotes.selectQuotesSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertQuotes: (quote) => db.run(quotes.insertQuotesSQL, [quote.text]),
+    deleteQuotes: run(quotes.deleteQuotesSQL),
+    deleteOneQuotes: run(quotes.deleteOnePartnersSQL, ['id']),
+    selectQuotes: all(quotes.selectQuotesSQL),
+    insertQuotes: run(quotes.insertQuotesSQL, ['text']),
 
-    deletePartners: () => db.run(partners.deletePartnersSQL),
-    deleteOnePartners: (partner) => db.run(partners.deleteOnePartnersSQL, [partner.id]),
-    selectPartners: () => new Promise((resolve) =>
-        db.all(partners.selectPartnersSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertPartners: (partner) => db.run(partners.insertPartnersSQL, [partner.title, partner.image, partner.link]),
+    deletePartners: run(partners.deletePartnersSQL),
+    deleteOnePartners: run(partners.deleteOnePartnersSQL, ['id']),
+    selectPartners: all(partners.selectPartnersSQL),
+    insertPartners: run(partners.insertPartnersSQL, ['title', 'image', 'link']),
 
-    deletePreviews: () => db.run(previews.deletePreviewsSQL),
-    deleteOnePreviews: (preview) => db.run(previews.deletePreviewsOneSQL, [preview.id]),
-    selectPreviews: () => new Promise((resolve) =>
-        db.all(previews.selectPreviewsSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertPreviews: (preview) => db.run(previews.insertPreviewsSQL, [preview.text, preview.preview, preview.link]),
+    deletePreviews: run(previews.deletePreviewsSQL),
+    deleteOnePreviews: run(previews.deletePreviewsOneSQL, ['id']),
+    selectPreviews: all(previews.selectPreviewsSQL),
+    insertPreviews: run(previews.insertPreviewsSQL, ['text', 'preview', 'link']),
 
-    deleteResidents: () => db.run(residents.deleteResidentsSQL),
-    deleteOneResidents: (resident) => db.run(residents.deleteResidentsOneSQL, [resident.id]),
-    selectResidents: () => new Promise((resolve) =>
-        db.all(residents.selectResidentsSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertResidents: (resident) => db.run(residents.insertResidentsSQL, [resident.name, resident.iconName, resident.href]),
+    deleteResidents: run(residents.deleteResidentsSQL),
+    deleteOneResidents: run(residents.deleteResidentsOneSQL, ['id']),
+    selectResidents: all(residents.selectResidentsSQL),
+    insertResidents: run(residents.insertResidentsSQL, ['name', 'iconName', 'href']),
 
-    deletePosters: () => db.run(posters.deletePostersSQL),
-    deleteOnePosters: (poster) => db.run(posters.deletePostersOneSQL, [poster.id]),
-    selectPosters: () => new Promise((resolve) =>
-        db.all(posters.selectPostersSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertPosters: (poster) => db.run(posters.insertPostersSQL, [poster.name, poster.iconName, poster.date, poster.description]),
+    deletePosters: run(posters.deletePostersSQL),
+    deleteOnePosters: run(posters.deletePostersOneSQL, ['id']),
+    selectPosters: all(posters.selectPostersSQL),
+    insertPosters: run(posters.insertPostersSQL, ['name', 'iconName', 'date', 'description']),
 
-    deleteUsers: () => db.run(users.deleteUsersSQL),
-    selectUsers: () => new Promise((resolve) =>
-        db.all(users.selectUsersSQL, (err, rows) => {resolve(rows)})
-    ),
-    insertUsers: (user) => db.run(users.insertUsersSQL, [user.name, user.pwdHash, user.role]),
-    findUsers: (name) => new Promise((resolve) =>
-        db.get(users.findUsersSQL, [name], (err, row) => {resolve(row)})
-    ),
+    deleteUsers: run(users.deleteUsersSQL),
+    selectUsers: all(users.selectUsersSQL),
+    insertUsers: run(users.insertUsersSQL, ['name', 'pwdHash', 'role']),
+    findUsers: get(users.findUsersSQL, ['name']),
 }
--- db/init_db.js
+++ db/init_db.js
@@ -2,37 +2,29 @@ const db = require('./db')
 const fs = require('fs')
 const path = require('path')
 const bcrypt = require('bcrypt');
+const util = require("util");
 require('dotenv').config({path: '.env.local'});
 
+const readFile = util.promisify(fs.readFile);
+async function initializeTable(create, clear, insert, filename) {
+    await create()
+    await clear()
+    if (!filename) return;
+    const file = await readFile(path.join(__dirname, filename))
+    const json = JSON.parse(file.toString())
+    await Promise.all(json.map(item => insert(item)))
+}
+
 (async () => {
-    await db.createQuotes();
-    await db.createPartners();
-    await db.createPreviews();
-    await db.createResidents();
-    await db.createPosters();
-    await db.createUsers();
-})().then(() => {
-    db.deleteQuotes()
-    fs.readFile(path.join(__dirname, '../data/quotes.json'),
-        (err, data) => JSON.parse(data.toString()).forEach(item => db.insertQuotes(item)))
+    await Promise.all([
+        initializeTable(db.createQuotes, db.deleteQuotes, db.insertQuotes, '../data/quotes.json'),
+        initializeTable(db.createPartners, db.deletePartners, db.insertPartners, '../data/partners.json'),
+        initializeTable(db.createPreviews, db.deletePreviews, db.insertPreviews, '../data/previews.json'),
+        initializeTable(db.createResidents, db.deleteResidents, db.insertResidents, '../data/residents.json'),
+        initializeTable(db.createPosters, db.deletePosters, db.insertPosters, '../data/posters.json'),
+        initializeTable(db.createUsers, db.deleteUsers, db.insertUsers),
+        bcrypt.hash(process.env.ADMIN_PASSWORD, +process.env.SALT_ROUNDS)
+            .then((value) => db.insertUsers({name: 'admin', role: 'admin', pwdHash: value}))
+    ])
+})()
 
-    db.deletePartners()
-    fs.readFile(path.join(__dirname, '../data/partners.json'),
-        (err, data) => JSON.parse(data.toString()).forEach(item => db.insertPartners(item)))
-
-    db.deletePreviews()
-    fs.readFile(path.join(__dirname, '../data/previews.json'),
-        (err, data) => JSON.parse(data.toString()).forEach(item => db.insertPreviews(item)))
-
-    db.deleteResidents()
-    fs.readFile(path.join(__dirname, '../data/residents.json'),
-        (err, data) => JSON.parse(data.toString()).forEach(item => db.insertResidents(item)))
-
-    db.deletePosters()
-    fs.readFile(path.join(__dirname, '../data/posters.json'),
-        (err, data) => JSON.parse(data.toString()).forEach(item => db.insertPosters(item)))
-
-    db.deleteUsers()
-    bcrypt.hash(process.env.ADMIN_PASSWORD, +process.env.SALT_ROUNDS)
-        .then((value) => db.insertUsers({name: 'admin', role: 'admin', pwdHash: value }))
-})
