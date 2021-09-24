3e496d3db080b0f4cd54b0665c2a1d61adaeab74
ekinohito <zhenyagur9@gmail.com>
2021-09-24 22:23:01 +0300

    minor fix

--- components/home_sections/quote_section.js
+++ components/home_sections/quote_section.js
@@ -8,8 +8,8 @@ export default function QuoteSection({timeout = 5000, ...props}) {
     const {quotes, error} = useGetQuotes(props.quotes);
     const [k, setK] = useState(0);
     const transitions = useTransition(k, {
-        from: {opacity: 0, top: 0},
-        enter: {opacity: 1, top: 0},
+        from: {opacity: 0},
+        enter: {opacity: 1},
         leave: {position: "absolute", left: 9999, opacity: 0},
         config: config.molasses,
     })
@@ -25,10 +25,10 @@ export default function QuoteSection({timeout = 5000, ...props}) {
     return (
         <MainColoredSection bgClass="bg-concert-photo">
             <div className="d-flex flex-row flex-nowrap">
-                {transitions(({opacity, position, top}, item) =>
+                {transitions(({opacity, position, left}, item) =>
                     <animated.div
                         key={item}
-                        style={{opacity: opacity, position, top}}
+                        style={{opacity: opacity, position, left}}
                     >
                         <div className="h2-text-lg h3-text text-white py-5 text-center" style={{lineHeight: "125%"}}>
                             "{quotes ? quotes[item]?.text: null}"
