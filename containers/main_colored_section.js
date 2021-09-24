70449b8431ea38c976abe8a3f45c321c8f3876b2
ekinohito <zhenyagur9@gmail.com>
2021-09-24 21:31:57 +0300

    minor fix

--- containers/main_colored_section.js
+++ containers/main_colored_section.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MainColoredSection({bgClass, children}) {
     return (
-        <div className={`py-5 ${bgClass}`} style={{"--bs-gradient": "url(assets/images/gradient.jpg)"}}>
+        <div className={`py-5 ${bgClass}`} style={{"--bs-gradient": 'url("assets/images/gradient.jpg")'}}>
             <div className="d-flex flex-column container">
                 {children}
             </div>
